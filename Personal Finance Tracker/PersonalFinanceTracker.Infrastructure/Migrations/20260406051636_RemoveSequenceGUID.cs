1bf92bac6b693264e5866e089e30f9c0f603fd7d
NamHa <work.namha@gmail.com>
2026-04-06 12:21:26 +0700

    Remove NEWSEQUENTIALID() default from Id columns

---
+++ Personal Finance Tracker/PersonalFinanceTracker.Infrastructure/Migrations/20260406051636_RemoveSequenceGUID.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinanceTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSequenceGUID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "UserRefreshTokens",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Budgets",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "NEWSEQUENTIALID()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "UserRefreshTokens",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Budgets",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "NEWSEQUENTIALID()",
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
